1bb1cf788f119195ef1474a1be095fefbf149338
arbaz <Pm9521263317@gmail.com>
2025-07-07 19:54:20 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,3 +6,17 @@ export const gradientColors = [
   "from-rose-500 to-pink-400",
   "from-indigo-500 to-purple-400",
 ];
+
+
+export
+  const dailySalesData = [
+    { name: "REMITTANCE", value: 4026.05 },
+    { name: "SETTLEMENT", value: 25000 },
+    { name: "AEPS", value: 0 },
+  ];
+export
+  const SalesActivity = [
+    { name: "REMITTANCE", value: 5026.05 },
+    { name: "SETTLEMENT", value: 2500 },
+    { name: "AEPS", value: 0 },
+  ];
