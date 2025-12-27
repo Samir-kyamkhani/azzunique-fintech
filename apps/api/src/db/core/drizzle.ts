2c7822359cb822fd8d9d29d58236325ecbce3674
Samir Khan <samirkayamkhani.dev@gmail.com>
2025-12-27 11:15:53 +0530

    name fixed

--- apps/api/src/db/core/drizzle.ts
+++ apps/api/src/db/core/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import { corePool } from './mysql';
 import * as schema from './schema';
 
-export const coreDb = drizzle(corePool, {
+export const CoreDbService = drizzle(corePool, {
   schema,
   mode: 'default',
 });
