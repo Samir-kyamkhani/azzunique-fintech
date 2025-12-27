113abde044df3cdf1544fa0746d06a9da568aea7
Samir Khan <samirkayamkhani.dev@gmail.com>
2025-12-27 10:51:38 +0530

    fixed

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,10 +6,14 @@ import { TenantDomainsModule } from './tenant-domains/tenant-domains.module';
 import { TenantPageModule } from './tenant-page/tenant-page.module';
 import { TenantSeoModule } from './tenant-seo/tenant-seo.module';
 import { TenantWebsiteModule } from './tenant-website/tenant-website.module';
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   controllers: [HealthController],
   providers: [HealthService],
-  imports: [TenantsModule, TenantDomainsModule, TenantPageModule, TenantSeoModule, TenantWebsiteModule],
+  imports: [TenantsModule, TenantDomainsModule, TenantPageModule, TenantSeoModule, TenantWebsiteModule, AuthModule, RolesModule, UsersModule, EmployeesModule],
 })
 export class AppModule {}
