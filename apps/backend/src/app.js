0ef3af1fa5a41dfa0dcd9be58b38110bf215b65b
Arbaz Khan <itsarbaz51@gmail.com>
2026-01-01 15:18:33 +0530

    add version

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.set('trust proxy', 1);
 
-
+app.use('/api/v1');
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static('public'));
