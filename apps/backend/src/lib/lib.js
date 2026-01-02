7c175f7624abcbdd5e54597df93966a2db799951
Arbaz Khan <itsarbaz51@gmail.com>
2026-01-02 12:32:44 +0530

    addd generate num

--- apps/backend/src/lib/lib.js
+++ apps/backend/src/lib/lib.js
@@ -127,3 +127,10 @@ export const generateTokens = (payload) => {
     refreshToken,
   };
 };
+
+export function generateNumber(prefix, length = 6) {
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+
+  return `${prefix}-${Math.floor(min + Math.random() * (max - min + 1))}`;
+}
