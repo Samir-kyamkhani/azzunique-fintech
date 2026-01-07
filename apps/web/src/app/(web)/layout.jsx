97263929ba6b28e09f87f331a97e295f59fc29a4
Samir Khan <samirkayamkhani.dev@gmail.com>
2026-01-07 12:52:05 +0530

    intial

--- apps/web/src/app/(web)/layout.jsx
+++ apps/web/src/app/(web)/layout.jsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import ReduxProvider from "@/providers/ReduxProvider";
