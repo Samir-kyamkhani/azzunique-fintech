90f2e23f441379cb728c5d8f008b8e3956b3dc27
Arbaz Khan <itsarbaz51@gmail.com>
2026-01-21 16:02:26 +0530

    set for temp

--- apps/backend/src/lib/auth.cookies.js
+++ apps/backend/src/lib/auth.cookies.js
@@ -1,13 +1,25 @@
+// export const cookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === 'production',
+//   sameSite: 'lax',
+//   maxAge: 24 * 60 * 60 * 1000, // 1 day
+// };
 export const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
+  secure: false,
   sameSite: 'lax',
   maxAge: 24 * 60 * 60 * 1000, // 1 day
 };
 
 export const refreshCookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
+  secure: false,
   sameSite: 'lax',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
+// export const refreshCookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === 'production',
+//   sameSite: 'lax',
+//   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+// };
