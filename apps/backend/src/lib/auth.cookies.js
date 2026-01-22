50cb6ab1041d4797b594e42c34b744de78fdda38
Arbaz Khan <itsarbaz51@gmail.com>
2026-01-22 15:59:53 +0530

    fix for production

--- apps/backend/src/lib/auth.cookies.js
+++ apps/backend/src/lib/auth.cookies.js
@@ -1,30 +1,31 @@
-export const cookieOptions = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
-  maxAge: 24 * 60 * 60 * 1000, // 1 day
-};
 // export const cookieOptions = {
 //   httpOnly: true,
-//   secure: false, // HTTP
+//   secure: process.env.NODE_ENV === 'production',
 //   sameSite: 'lax',
-//   path: '/', 
-//   domain: '.147.93.20.127.sslip.io',
-//   maxAge: 24 * 60 * 60 * 1000,
+//   maxAge: 24 * 60 * 60 * 1000, // 1 day
 // };
 
-// export const refreshCookieOptions = {
-//   httpOnly: true,
-//   secure: false,
-//   sameSite: 'lax',
-//   path: '/',
-//   domain: '.147.93.20.127.sslip.io',
-//   maxAge: 7 * 24 * 60 * 60 * 1000,
-// };
+export const cookieOptions = {
+  httpOnly: true,
+  secure: false, // HTTP
+  sameSite: 'lax',
+  path: '/',
+  domain: '.147.93.20.127.sslip.io',
+  maxAge: 24 * 60 * 60 * 1000,
+};
 
 export const refreshCookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
+  secure: false,
   sameSite: 'lax',
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  path: '/',
+  domain: '.147.93.20.127.sslip.io',
+  maxAge: 7 * 24 * 60 * 60 * 1000,
 };
+
+// export const refreshCookieOptions = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === 'production',
+//   sameSite: 'lax',
+//   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+// };
